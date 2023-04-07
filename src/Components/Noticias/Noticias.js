b678c23a5e89b63c0c01ab0a51c2f7487f26ee85
Andrés Del Fabro <andresdelfabro99@gmail.com>
2023-04-07 18:13:02 -0300

    estructuraNoticia

---
+++ src/Components/Noticias/Noticias.js
@@ -0,0 +1,14 @@
+import styles from './Noticias.module.css'
+
+export default function Noticias() {
+  return (
+    <div class={styles.container}>
+    <div class={styles["Noticia-"]}> <h1>noticias</h1></div>
+    <div class={styles.N1}><h1>noticias</h1></div>
+    <div class={styles.N2}><h1>noticias</h1></div>
+    <div class={styles.N3}><h1>noticias</h1></div>
+    <div class={styles.N4}><h1>noticias</h1></div>
+    <div class={styles.N5}><h1>noticias</h1></div>
+  </div>
+  )
+}
