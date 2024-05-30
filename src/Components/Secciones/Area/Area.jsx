7ca146538338dd5784e16c3de67f3bb6f834b5a2
Emanuel85 <emanuelquintanilla85@gmail.comm>
2024-05-30 10:32:17 -0300

    fix: seccion y areas del colegio

--- src/Components/Secciones/Area/Area.jsx
+++ src/Components/Secciones/Area/Area.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import styles from './Area.module.css'
-const Area = ({ areaNum }) => {
-  let name = undefined
-  if (areaNum === 1) {
-    name =("EVENTOS Y REUNIONES");
-  } else if (areaNum === 2) {
-    name = "ADMINISTRATIVA";
-  } else if (areaNum === 3) {
-    name =("ACTIVIDADES EXTRACURRICULARES");
-  } else if (areaNum === 4) {
-    name =("CIENCIA Y TECNOLOGIA");
-  } else if (areaNum === 5) {
-    name =("INSTALACIONES DEPORTIVAS");
-  } else if (areaNum === 6) {
-    name =("SOCIAL Y CULTURAL");
-  } else {
-    name =("Opción inválida");
+import itemSeccions from '@/pages/api/itemSeccions';
+
+const Area = ({ areaNum, setArea }) => {
+  console.log(areaNum)
+
+  const resulFilter = itemSeccions.filter(item => item.id === areaNum)
+
+  const handleCancel = () => {
+    setArea(0)
   }
 
-
-  
   return (
-    <div className={styles.container}>
-  <div className={styles.img1}></div>
-  <div className={styles.img2}></div>
-  <div className={styles.area2}>
-    
-  </div>
-  <div className={styles.area1}>
-  <h2>{name}</h2>
-  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium aperiam cum sint assumenda tempore nam quo perspiciatis est. Adipisci doloribus veritatis magnam porro explicabo ducimus, dicta repudiandae et aut. Voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eveniet quasi quo nulla dolores nam, ea debitis, voluptatum itaque consequuntur ullam expedita recusandae quas voluptate ex beatae atque eum magni.</h3>
-  </div>
-</div>
+    <>
+      {resulFilter?.map((item) =>
+        <div className={styles.container}>
+          <div className={styles.containerImage}>
+            <div className={styles.img1}></div>
+            <div className={styles.img2}></div>
+          </div>
+          <div className={styles.area}>
+            <div className={styles.areaTitle}>
+              <h2 >{item.title}</h2>
+            </div>
+            <div className={styles.areaText}>
+              <h3>{item.text}</h3>
+              <div className={styles.areaButton}>
+                <button onClick={() => handleCancel}>CERRAR</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/Components/Secciones/Seccions.jsx
+++ src/Components/Secciones/Seccions.jsx
@@ -1,10 +1,13 @@
 import styles from "./Seccions.module.css";
 import { useState } from "react";
 import Area from "./Area/Area";
+import itemSeccions from "@/pages/api/itemSeccions";
 
 export default function Seccions() {
   const [area, setArea] = useState(0);
 
+
+  console.log(area,'SECCIONES')
   const handleClick = () => {
     setArea(0);
   };
@@ -14,36 +17,17 @@ export default function Seccions() {
   if (area === 0) {
     componentToShow = (
       <div className={styles.container}>
-        <div id="area1" className={styles.area1} onClick={() => setArea(1)}>
-          <h1>
-            EVENTOS <br/> Y <br/> REUNIONES
-          </h1>
-        </div>
-        <div id="area2" className={styles.area2} onClick={() => setArea(2)}>
-          <h1>ADMINISTRATIVA</h1>
-        </div>
-        <div id="area3" className={styles.area3} onClick={() => setArea(3)}>
-          <h1>ACTIVIDADES EXTRACURRICULARES</h1>
-        </div>
-        <div id="area4" className={styles.area4} onClick={() => setArea(4)}>
-          <h1>CIENCIA Y TECNOLOGIA</h1>
-        </div>
-        <div id="area5" className={styles.area5} onClick={() => setArea(5)}>
-          <h1>
-            INSTALACIONES
-            <br />
-            DEPORTIVAS
-          </h1>
-        </div>
-        <div id="area6" className={styles.area6} onClick={() => setArea(6)}>
-          <h1>SOCIAL Y CULTURAL</h1>
-        </div>
+        {
+          itemSeccions?.map((item, index) =>
+            <div key={index} className={styles[`area${item.id}`]} onClick={() => setArea(item.id)} ><h1> {item.title} </h1></div>
+          )}
       </div>
     );
   } else {
     componentToShow = (
+     
       <div className={styles.parent} onClick={handleClick}>
-        <Area areaNum={area} className={styles.area} />
+        <Area areaNum={area} className={styles.area} setArea={setArea}/>
       </div>
     );
   }
