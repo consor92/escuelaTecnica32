e33a82471cd9b1aa4e4b4ed1ccb1662760e0b480
emanuelquintanilla85@gmail.com <emanuelquintanilla85@gmail.comm>
2025-04-04 22:12:31 -0300

    fix: redireccionamiento desde una seccion

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,14 +2,29 @@ import React, { useState } from 'react'
 import Style from './NavBar.module.css'
 import Link from 'next/link'
 import { HiMenu } from 'react-icons/hi'
-
+import { useRouter } from 'next/router'
 
 const NavBar = ({ page }) => {
   const [sideBarOpen, setSideBarOpen] = useState(false)
-
+  const router = new useRouter()
   const handleOpenSideBar = () => {
-    console.log('ENTRE')
     setSideBarOpen(!sideBarOpen)
+  }
+
+  const handleNavigateToAnchor = async (hash) => {
+    setSideBarOpen(false)
+
+    if (router.pathname !== '/') {
+      await router.push(`/#${hash}`)
+    } else {
+      // Si ya estás en /, simplemente hacé scroll
+      const element = document.getElementById(hash)
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
+      } else {
+        router.push(`/#${hash}`)
+      }
+    }
   }
 
   return (
@@ -42,29 +57,36 @@ const NavBar = ({ page }) => {
         </div>
         <div className={Style.container__navBarBottom}>
           <nav>
-            {page === 'home' ?
-              <a href='#disciplines' onClick={handleOpenSideBar}>ESPECIALIDADES</a>
-              :
-              <Link href='/#disciplines' onClick={handleOpenSideBar}> ESPECIALIDADES</Link>
-            }
-
-            <a href='#inscripciones' onClick={handleOpenSideBar}>INSCRIPCIONES</a>
-
-            <Link href='#sections' onClick={handleOpenSideBar} >
-              INFRAESTRUCTURA
-            </Link>
-            
-            <a href='#cooperadora' onClick={handleOpenSideBar}>COOPERADORA</a>
+            <button onClick={() => handleNavigateToAnchor('disciplines')}>ESPECIALIDADES</button>
+            <button onClick={() => handleNavigateToAnchor('inscripciones')}>INSCRIPCIONES</button>
+            <button onClick={() => handleNavigateToAnchor('sections')}>INFRAESTRUCTURA</button>
+            <button onClick={() => handleNavigateToAnchor('cooperadora')}>COOPERADORA</button>
 
           </nav>
 
         </div>
       </div>
       <button onClick={handleOpenSideBar} className={Style.container__sideBar}>
-        <HiMenu />
+        <HiMenu size={40} />
       </button>
     </div>
   )
 }
 
 export default NavBar
+
+
+
+// {page === 'home' ?
+//   <a href='#disciplines' onClick={handleOpenSideBar}>ESPECIALIDADES</a>
+//   :
+//   <Link href='/#disciplines' onClick={handleOpenSideBar}> ESPECIALIDADES</Link>
+// }
+
+// <a href='#inscripciones' onClick={handleOpenSideBar}>INSCRIPCIONES</a>
+
+// <Link href='#sections' onClick={handleOpenSideBar} >
+//   INFRAESTRUCTURA
+// </Link>
+
+// <a href='#cooperadora' onClick={handleOpenSideBar}>COOPERADORA</a>
